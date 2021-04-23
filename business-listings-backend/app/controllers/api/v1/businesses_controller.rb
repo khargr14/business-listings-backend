2407227041b5396913494926c2b256d0967fbb05
khargr14 <kay_hargrove@yahoo.com>
2021-04-22 22:29:57 -0400

    add crud to controllers

--- business-listings-backend/app/controllers/api/v1/businesses_controller.rb
+++ business-listings-backend/app/controllers/api/v1/businesses_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::BusinessesController < ApplicationController
+
+    def index
+        businesses = Business.all
+        
+        render json: businesses
+      end
+    
+      def create
+        business = Business.new(business_params)
+        if business.save
+          render json: business
+        else
+          render json: {error: 'Error creating business'}
+        end
+      end
+    
+      def show
+        business = Business.find(params[:id])
+        render json: business
+      end
+    
+      def update
+        business = Business.find(params[:id])
+        business.update(business_params)
+        render json: business
+      end
+    
+      def destroy
+        business = Business.find(params[:id])
+        business.destroy
+        # render json: business.all
+      end
+    
+      private
+    
+      def business_params
+        params.require(:business).permit(:name, :number, :email, :industry)
+      end
 end
--- business-listings-backend/app/controllers/api/v1/contacteds_controller.rb
+++ business-listings-backend/app/controllers/api/v1/contacteds_controller.rb
@@ -1,2 +1,45 @@
 class Api::V1::ContactedsController < ApplicationController
+    # before_action :set_contacted
+
+    def index
+        contacteds = @business.contacteds
+        
+        render json: contacteds
+      end
+    
+      def create
+        contacted = @business.contacteds.new(contacted_params)
+        if contacted.save
+          render json: @business
+        else
+          render json: {error: 'Error creating contacted'}
+        end
+      end
+    
+      def show
+        contacted = @business.contacteds.find_by(id: params[:id])
+        render json: contacted
+      end
+    
+      def update
+        contacted = Contacted.find(params[:id])
+        contacted.update(contacted_params)
+        render json: contacted
+      end
+    
+      def destroy
+        contacted = @business.contacteds.find_by(id: params[:id])
+        contacted.destroy
+        render json: @business
+      end
+    
+      private
+    
+      def set_business
+        @business = Business.find(params[:business_id])
+      end
+    
+      def contacted_params
+        params.require(:contacted).permit(:business_id, :name, :number, :email, :date, :message)
+      end
 end
